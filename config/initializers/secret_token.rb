73f3a170f0e170d73ddc8074a3b53f3f31fb8219
Elliot Nash <Elliot@venturemedias.com>
2012-04-10 17:40:59 -0400

    all

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Elliotnash::Application.config.secret_token = 'edd4bbb905ea2807b15cc53a5509a743ea5bb493f25fb2fa74e37cd8d606a5fc290a8ba562fb727df0c7ca43a217d31232a3b4387eb8f49c70fdadcebe2d956a'
